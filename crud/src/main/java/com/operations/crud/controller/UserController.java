6350be66ddf60dc278d33bf9d660bfd5006fa6e7
naveentestsigma <127829040+naveentestsigma@users.noreply.github.com>
2023-03-21 11:49:26 +0530

    Add files via upload

---
+++ crud/src/main/java/com/operations/crud/controller/UserController.java
@@ -0,0 +1,84 @@
+package com.operations.crud.controller;
+
+import lombok.AllArgsConstructor;
+import com.operations.crud.entity.User;
+import com.operations.crud.service.UserService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController //plain api endpoints (no html returns) , controller used for html or xml returns view(UI components).
+@AllArgsConstructor
+@RequestMapping("api/users")
+public class UserController {
+
+    private UserService userService;
+
+    // build create User REST API
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<User> createUser(@RequestBody User user){
+        User savedUser = userService.createUser(user);
+        return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
+    }
+
+    // build get user by id REST API
+    // http://localhost:8080/api/users/1
+    @GetMapping("{id}")
+    public ResponseEntity<User> getUserById(@PathVariable("id") Long userId){
+        User user = userService.getUserById(userId);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+    // Build Get All Users REST API
+    // http://localhost:8080/api/users
+    @CrossOrigin("http://localhost:4200/")
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers(){
+        List<User> users = userService.getAllUsers();
+        return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+
+    // Build Update User REST API
+    @PutMapping("{id}")
+    // http://localhost:8080/api/users/1
+    public ResponseEntity<User> updateUser(@PathVariable("id") Long userId,
+                                           @RequestBody User user){
+        user.setId(userId);
+        User updatedUser = userService.updateUser(user);
+        return new ResponseEntity<>(updatedUser, HttpStatus.OK);
+    }
+
+
+
+    // Build Delete User REST API
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> deleteUser(@PathVariable("id") Long userId){
+        userService.deleteUser(userId);
+        return new ResponseEntity<>("User successfully deleted!", HttpStatus.OK);
+    }
+
+}
+
+@Controller
+class view {
+    private UserService userService;
+    view(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/view/users")
+    public String index(Model model) {
+        model.addAttribute("allemplist", userService.getAllUsers());
+
+        return "index";
+    }
+}
