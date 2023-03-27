7062199d943721042bcfe1e326052636dcddf9ed
naveentestsigma <naveenkumar.chandrasekar@testsigma.com>
2023-03-27 12:17:27 +0530

    mapper and dto

---
+++ MapperandDTO/src/main/java/com/operations/crud/controller/UserDTO.java
@@ -0,0 +1,15 @@
+package com.operations.crud.controller;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class UserDTO {
+    private String firstName;
+    private String lastName;
+
+}
